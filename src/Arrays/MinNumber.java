159c4d0796861c089020ceab3e7161328fa9909c
Omm Prakash Das <ommprakash778896@gmail.com>
2026-02-18 18:25:48 +0530

    Minimum number

---
+++ src/Arrays/MinNumber.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class MinNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter numbers: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = Integer.MAX_VALUE;
+
+        for (int ele : arr) {
+            max = Math.min(ele, max);
+        }
+
+        System.out.println("Maximum number is: " + max);
+    }
+}
