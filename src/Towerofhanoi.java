15fd5c6c7cc73e14e1cf75061966e843c36f4170
Omm Prakash Das <ommprakash778896@gmail.com>
2026-04-20 20:50:28 +0530

    essage

--- src/Towerofhanoi.java
+++ src/Towerofhanoi.java
@@ -1,7 +1,7 @@
 public class Towerofhanoi {
 public static void towerofhanoi(int n,String src,String helper,String dest){
     if(n==1){
-       System.out.println("transferdisk "+ n +" form "+ src +" to "+dest);
+        System.out.println("transferdisk "+ n +" form "+ src +" to "+dest);
         return;
     }
 
@@ -10,7 +10,7 @@ public static void towerofhanoi(int n,String src,String helper,String dest){
     towerofhanoi(n-1,helper,src,dest);
 
 } public static void main(String[] args) {
-        int n = 3;
+        int n = 4;
         towerofhanoi(n, "S", "H", "D");
 
     }
