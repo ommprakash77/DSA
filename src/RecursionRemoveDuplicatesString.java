ee3e929469fbbad7d4b580b6051d0ee5a7d4d89b
Omm Prakash Das <ommprakash778896@gmail.com>
2026-04-24 22:44:59 +0530

    message

---
+++ src/RecursionRemoveDuplicatesString.java
@@ -0,0 +1,23 @@
+public class RecursionRemoveDuplicatesString {
+    public static boolean[]map=new boolean[26];
+
+    public static void removeDuplicates(String str,int idx,String newString){
+        if(idx==str.length()){
+            System.out.println(newString);
+            return ;
+        }
+        char currchar=str.charAt(idx);
+        if(map[currchar-'a']) {
+            removeDuplicates(str, idx + 1, newString);
+        }else{
+            newString+=currchar;
+            map[currchar-'a']=true;
+            removeDuplicates(str,idx+1,newString);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str="abbccda";
+        removeDuplicates(str,0,"");
+    }
+}
