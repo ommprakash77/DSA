188693eea5754e323d8bf489caca139d762191b6
Omm Prakash Das <ommprakash778896@gmail.com>
2026-05-26 22:58:32 +0530

    message

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a, b;
+        char operator;
+
+        System.out.print("Enter first number:");
+        a = sc.nextDouble();
+
+        System.out.print("Enter operator(+,-,*,/):");
+        operator = sc.next().charAt(0);
+
+        System.out.print("Enter second number:");
+        b = sc.nextDouble();
+
+        if (operator == '+') {
+            System.out.println("Result=" + (a + b));
+    }else if(operator == '-') {
+            System.out.println("Result=" + (a - b));
+        }else if(operator =='*') {
+            System.out.println("Result=" + (a * b));
+        }else if(operator =='/') {
+            System.out.println("Result=" + (a / b));
+        }else{
+            System.out.println("Invalied operator");
+
+        }
+
+    }
+
+}
