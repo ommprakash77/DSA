1bbefd5fb2674f00a3abc0fa418b88bb80855fd5
Omm Prakash Das <ommprakash778896@gmail.com>
2026-05-28 15:41:26 +0530

    message

---
+++ src/Recursion1To3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Recursion1To3 {
+    public static void printNum(int n){
+        if(n<=3){
+            System.out.println(n);
+            printNum(n+1);
+        }
+    }
+    public static void main(String[] args) {
+        printNum(1);
+
+    }
+}
