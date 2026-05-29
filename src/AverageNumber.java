aeb2cb39476e69ebd50077dc9f4257b0da0574f2
Omm Prakash Das <ommprakash778896@gmail.com>
2026-05-29 17:21:03 +0530

    message

---
+++ src/AverageNumber.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class AverageNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a, b, c, d, e, sum, average;
+
+        System.out.print("Enter the First number:");
+        a = sc.nextDouble();
+
+        System.out.print("Enter the Second number:");
+        b = sc.nextDouble();
+
+        System.out.print("Enter the Third number:");
+        c = sc.nextDouble();
+
+        System.out.print("Enter the Four number:");
+        d = sc.nextDouble();
+
+        System.out.print("Enter the Five number:");
+        e = sc.nextDouble();
+
+        sum = a + b + c + d + e;
+        average = sum / 5.0;
+
+        System.out.print("Average number:" + average);
+
+
+    }
+}
