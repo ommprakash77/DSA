ae46cd4d61223358e38e2deaba068d51d7f5a2f3
Omm Prakash Das <ommprakash778896@gmail.com>
2026-05-31 23:53:07 +0530

    message

---
+++ src/AreaOfCircle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class AreaOfCircle {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            double radius, area;
+
+            System.out.print("Enter radius: ");
+            radius = sc.nextDouble();
+
+            area = 3.14 * radius * radius;
+
+            System.out.println("Area of Circle = " + area);
+
+        }
+    }
+
