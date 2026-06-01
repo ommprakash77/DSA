d465b6e7ee5a7f8fe70d91d989f1f3230907cecc
Omm Prakash Das <ommprakash778896@gmail.com>
2026-06-01 15:45:25 +0530

    message

---
+++ src/IfElseLadder.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class IfElseLadder {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=10;
+        if(a>5) {
+            System.out.println("A");
+        }else if(a>8) {
+            System.out.println("B");
+        }else{
+            System.out.println("C");
+        }
+    }
+}
