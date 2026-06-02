677c46da2716d0e2022e6ad09f979a9ce74160b4
Omm Prakash Das <ommprakash778896@gmail.com>
2026-06-02 23:59:34 +0530

    message

---
+++ src/SeniorLeapyear.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class SeniorLeapyear {
+
+    public class LeapYear {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            int year;
+
+            System.out.print("Enter a year: ");
+            year = sc.nextInt();
+
+            if(year % 4 == 0) {
+                System.out.println("Leap Year");
+            } else {
+                System.out.println("Not a Leap Year");
+            }
+
+        }
+    }
+}
