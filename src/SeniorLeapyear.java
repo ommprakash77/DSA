192384469faaed466aab630819f34e12f70e1a21
Omm Prakash Das <ommprakash778896@gmail.com>
2026-06-03 00:05:20 +0530

    message

--- src/SeniorLeapyear.java
+++ src/SeniorLeapyear.java
@@ -1,8 +1,6 @@
 import java.util.*;
 public class SeniorLeapyear {
 
-    public class LeapYear {
-
         public static void main(String[] args) {
 
             Scanner sc = new Scanner(System.in);
@@ -20,4 +18,3 @@ public class SeniorLeapyear {
 
         }
     }
-}
