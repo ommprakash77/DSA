feae9b6b3e9ac1d694efa93600dc043756e3af73
Omm Prakash Das <ommprakash778896@gmail.com>
2026-06-09 23:22:15 +0530

    message

---
+++ src/FactorialProgram.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class FactorialProgram{
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            int num;
+            int fact = 1;
+
+            System.out.print("Enter a number: ");
+            num = sc.nextInt();
+
+            for(int i = 1; i <= num; i++) {
+                fact = fact * i;
+            }
+
+            System.out.println("Factorial = " + fact);
+        }
+    }
+
