8c50dc18f0af25a76618aa73af360a1978711623
Omm Prakash Das <ommprakash778896@gmail.com>
2026-07-12 22:45:27 +0530

    message

---
+++ src/LogicalQuestion7.java
@@ -0,0 +1,12 @@
+public class LogicalQuestion7 {
+    public static void main(String[] args) {
+        Integer a=127;
+        Integer b=127;
+
+        Integer c=128;
+        Integer d=128;
+
+        System.out.println(a==b);
+        System.out.println(c==d);
+    }
+}
