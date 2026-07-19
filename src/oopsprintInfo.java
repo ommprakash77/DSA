ada80a4ee99616d86e2c0754362daea603eca643
Omm Prakash Das <ommprakash778896@gmail.com>
2026-07-19 22:47:11 +0530

    message

---
+++ src/oopsprintInfo.java
@@ -0,0 +1,19 @@
+public class oopsprintInfo {
+    public static void main(String[] args) {
+
+        class student{
+            String name;
+            int age;
+
+            public void printInfo(){
+                System.out.println(this.name);
+                System.out.println(this.age);
+            }
+        }
+        student s1=new student();
+        s1.name="omm";
+        s1.age=19;
+
+        s1.printInfo();
+    }
+}
