86674f2d0002e96b8304c8909e1d940abe99048a
Omm Prakash Das <ommprakash778896@gmail.com>
2026-07-29 23:20:11 +0530

    message

---
+++ src/AccessModifiersPublic.java
@@ -0,0 +1,11 @@
+public class AccessModifiersPublic {
+    public static void main(String[] args) {
+        Package bank;
+        class Account{
+            public String name;
+            }
+            Account account1=new Account();
+            account1.name="omm";
+        System.out.println(account1.name);
+        }
+    }
