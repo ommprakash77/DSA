5dd9c22182d7a2269169f83803ac8e796417173d
Omm Prakash Das <ommprakash778896@gmail.com>
2026-08-06 22:59:55 +0530

    message

--- src/Halfrectangle.java
+++ src/Halfrectangle.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Halfrectrangle {
+public class Halfrectangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value n:");
