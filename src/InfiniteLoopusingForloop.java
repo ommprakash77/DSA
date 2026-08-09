4bc2d0bb9f3579dbf87c0acab36274bb66f7b973
Omm Prakash Das <ommprakash778896@gmail.com>
2026-08-09 10:33:59 +0530

    message

---
+++ src/InfiniteLoopusingForloop.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class InfiniteLoopusingForloop {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;true;i++){
+            System.out.println("hey");
+        }
+    }
+}
