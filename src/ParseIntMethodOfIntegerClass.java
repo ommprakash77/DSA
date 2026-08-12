dc73146fe0707c5fe444a83519e7e056e66693c5
Omm Prakash Das <ommprakash778896@gmail.com>
2026-08-12 23:44:51 +0530

    message

---
+++ src/ParseIntMethodOfIntegerClass.java
@@ -0,0 +1,7 @@
+public class ParseIntMethodOfIntegerClass {
+    public static void main(String[] args) {
+        String name="143";
+        int number=Integer.parseInt(name);
+        System.out.println(number);
+    }
+}
