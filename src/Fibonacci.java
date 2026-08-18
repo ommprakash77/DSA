bb7bb707f8ed7c20696773b66a08a076dd5c28be
Omm Prakash Das <ommprakash778896@gmail.com>
2026-08-18 23:31:12 +0530

    message

---
+++ src/Fibonacci.java
@@ -0,0 +1,23 @@
+public class Fibonacci {
+
+        public static int fib(int n) {
+
+            if (n <= 1) {
+                return n;
+            }
+
+            int a = 0, b = 1;
+
+            for (int i = 1; i < n; i++) {
+                int c = a + b;
+                a = b;
+                b = c;
+            }
+
+            return b;
+        }
+
+        public static void main(String[] args) {
+            System.out.println(fib(5));
+        }
+    }
