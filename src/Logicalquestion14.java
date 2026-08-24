a14f43fd24d8aee4810b36c0a02c96ea88300a92
Omm Prakash Das <ommprakash778896@gmail.com>
2026-08-24 23:51:50 +0530

    message

---
+++ src/Logicalquestion14.java
@@ -0,0 +1,10 @@
+public class Logicalquestion14 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+
+        int result = (a | b) ^ (a & b);
+
+        System.out.println(result);
+    }
+}
