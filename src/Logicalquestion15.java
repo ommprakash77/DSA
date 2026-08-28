1f661c7928261fab0e1e2ba58f42e7876c8c7232
Omm Prakash Das <ommprakash778896@gmail.com>
2026-08-28 22:33:55 +0530

    message

---
+++ src/Logicalquestion15.java
@@ -0,0 +1,13 @@
+public class Logicalquestion15 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        if (a < b && b < c) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
