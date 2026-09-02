b5685db812c36059add0596d7da3237dd0a63c0c
Omm Prakash Das <ommprakash778896@gmail.com>
2026-09-02 23:51:04 +0530

    message

---
+++ src/Logicalquestion17.java
@@ -0,0 +1,13 @@
+public class Logicalquestion17 {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 20;
+
+        if (x++ > 10 && ++y > 20) {
+            System.out.println(x + " " + y);
+        } else {
+            System.out.println(x + " " + y);
+        }
+    }
+    }
