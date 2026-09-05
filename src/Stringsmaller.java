953c77aaf7ea126bf766a1ec70e97410f39a97e3
Omm Prakash Das <ommprakash778896@gmail.com>
2026-09-05 22:19:54 +0530

    message

---
+++ src/Stringsmaller.java
@@ -0,0 +1,7 @@
+public class Stringsmaller {
+    public static void main(String[] args) {
+        String str="OMM PRAKASH DAS";
+        String lower=str.toLowerCase();
+        System.out.println(lower);
+    }
+}
