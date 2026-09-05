d40a323af609ee795d4780a42fdb08ba664b8194
Omm Prakash Das <ommprakash778896@gmail.com>
2026-09-05 22:15:21 +0530

    message

---
+++ src/Stringcapital.java
@@ -0,0 +1,7 @@
+public class Stringcapital {
+    public static void main(String[] args) {
+        String str="omm prakash das";
+        String capital=str.toUpperCase();
+        System.out.println(capital);
+    }
+}
