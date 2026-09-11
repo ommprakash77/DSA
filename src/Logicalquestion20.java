837762340f13656c30350ca31a79ffcf195d99b8
Omm Prakash Das <ommprakash778896@gmail.com>
2026-09-11 23:49:10 +0530

    message

---
+++ src/Logicalquestion20.java
@@ -0,0 +1,11 @@
+public class Logicalquestion20 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 4;
+
+        int result = a % b + a / b * 2;
+
+        System.out.println(result);
+    }
+}
