d413c6612472f2461d0c8f2856a12889f22a885b
Omm Prakash Das <ommprakash778896@gmail.com>
2026-09-12 23:49:16 +0530

    message

---
+++ src/Logicalquestion21.java
@@ -0,0 +1,9 @@
+public class Logicalquestion21 {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+
+        System.out.println(a < b && b > 8 || a == 10);
+    }
+}
