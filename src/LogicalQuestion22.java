5a3b10a400601e33d45479e46c91303feb2851dd
Omm Prakash Das <ommprakash778896@gmail.com>
2026-09-13 23:49:15 +0530

    message

---
+++ src/LogicalQuestion22.java
@@ -0,0 +1,10 @@
+public class LogicalQuestion22 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 3;
+
+        int result = ++a + b-- * 2;
+
+        System.out.println(result);
+    }
+}
