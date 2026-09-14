df63c80cafe76c1d7fa5787b164992fd4eea22d3
Omm Prakash Das <ommprakash778896@gmail.com>
2026-09-14 23:54:05 +0530

    message

---
+++ src/Logicalquestion23.java
@@ -0,0 +1,15 @@
+public class Logicalquestion23 {
+    public static void main(String[] args) {
+
+
+        int a = 10;
+        int b = 5;
+
+        a += b;
+        b *= 2;
+        a -= b;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
