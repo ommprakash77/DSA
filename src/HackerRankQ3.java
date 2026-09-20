a8543674c4259dc682da758cf72c0e92dfd8f330
Omm Prakash Das <ommprakash778896@gmail.com>
2026-09-20 23:17:00 +0530

    message

--- src/HackerRankQ3.java
+++ src/HackerRankQ3.java
@@ -1,8 +1,6 @@
 import java.util.*;
 
 public class HackerRankQ3 {
-
-
         public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
             int N=sc.nextInt();
---
+++ src/HackerRankQ4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class HackerRankQ4 {
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int i = scan.nextInt();
+
+            double j=scan.nextDouble();
+            scan.nextLine();
+            String str=scan.nextLine();
+
+            // Write your code here.
+
+            System.out.println("String: " + str);
+            System.out.println("Double: " + j);
+            System.out.println("Int: " + i);
+        }
+    }
+
+
