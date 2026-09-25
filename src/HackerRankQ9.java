f2155b7b313587091e9eb6116ee163fd4486b431
Omm Prakash Das <ommprakash778896@gmail.com>
2026-09-25 23:43:47 +0530

    message

---
+++ src/HackerRankQ9.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class HackerRankQ9 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i=1;
+        while(sc.hasNextLine()){
+            String a=sc.nextLine();
+            System.out.println(i+" "+a);
+            i++;
+        }
+    }
+}
